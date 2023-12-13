6e82ff99e7ce399408f6ec7117020bf7a5347d5c
Montaguine <yuria.cifuentes@gmail.com>
2023-12-13 16:07:27 -0300

    Criou a classe YuriClasse

--- Ada_AtividadeGit/Program.cs
+++ Ada_AtividadeGit/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Yuri");
+            YuriClasse.YuriMetodo();
         }
     }
 }
