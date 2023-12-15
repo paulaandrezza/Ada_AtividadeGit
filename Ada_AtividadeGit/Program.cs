8f3226db34f01d14ca226e6f0d097af5e9f58ad2
Luana <luhyxi@gmail.com>
2023-12-15 11:04:01 -0300

    Cool

--- Ada_AtividadeGit/Program.cs
+++ Ada_AtividadeGit/Program.cs
@@ -5,8 +5,6 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Luana Daorinha");
-            Console.WriteLine("Yuri");
         }
     }
 }
