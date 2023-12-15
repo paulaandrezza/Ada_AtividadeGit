9a995c82f5e0078da9aa4ee62fc96dd15dbd6be7
Montaguine <yuria.cifuentes@gmail.com>
2023-12-15 12:12:27 -0300

    Ajustes gerais

--- Ada_AtividadeGit/ConversaoRealDolar.cs
+++ Ada_AtividadeGit/ConversaoRealDolar.cs
@@ -13,7 +13,7 @@ namespace Ada_AtividadeGit
         /// </summary>
         /// <param name="valorReal"></param>
         /// <returns></returns>
-        public double ConverteRealParaDolarCanadense(double valorReal)
+        public double Converter(double valorReal)
         {
             double taxaConversao = 3.69;
             double valorDolarCanadense = valorReal / taxaConversao;
--- Ada_AtividadeGit/Program.cs
+++ Ada_AtividadeGit/Program.cs
@@ -4,17 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("\t\tCONVERSÃO DE MOEDAS");
-            Console.WriteLine("Grupo: Laura, Paula, Charles, Luana e Yuri\n");
-
-            ConversaoRealEuro converterEuro = new ConversaoRealEuro();
-            double valorConvertido = converterEuro.ConversaoEuro();
-
-            ConversaoRealPeso converterPesoArgentino = new ConversaoRealPeso();
-            double valorConvertido = converterPesoArgentino.ConversaoPesoArgentino();          
-
-            ConversaoRealIene conversor = new ConversaoRealIene();
-            conversor.Converter();
+            var menu = new InterfaceUsuario();
+            menu.MenuUsuario();
         }
     }
 }
