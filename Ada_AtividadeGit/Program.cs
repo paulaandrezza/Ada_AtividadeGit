c74a5eddb0161afad36cccb4fa05ddefe1a2005d
paulaandrezza <paulaandrezza25@gmail.com>
2023-12-15 11:09:57 -0300

    remove comented

--- Ada_AtividadeGit/Program.cs
+++ Ada_AtividadeGit/Program.cs
@@ -13,10 +13,6 @@
 
             ConversaoRealIene conversor = new ConversaoRealIene();
             conversor.Converter();
-
-            //double real = 1;
-            //double euro = YuriClasse.conversaoEuro(real);
-            //double pesoArgentino = LauraClasse.conversaoPesoArgentino(real);
         }
     }
 }
